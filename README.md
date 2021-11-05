726f5fec3ede0e4053454d3d1413ddbfc08d3629
ihsan <ihsan@kominfo.surabaya.go.id>
2021-11-06 01:45:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Zulvia & Ihsan Wedding Website
+
+
+Features:
+- Landing Page, with features like:
+ - Countdown Timer
+ - Live Button
+ - Floating Music
+ - Photo Gallery
+ - Pre-wedding Video 
+ - And more..
+- also, this web-app has QR Guest Generator
+- and Guest Tikcet Viewer
+
+Tech Stack:
+- GatsbyJs
+
+Credits
+- Original Landing Page Template (https://freehtml5.co/)
+- Icon Assets (https://www.flaticon.com/packs/covid-protection-measures-5)
+
+Thanks!
---
+++ src/components/WishesSection/wishlist-data.js
@@ -0,0 +1,24 @@
+import alvia from './assets/w-alvia.jpg';
+import girl from './assets/girl.png';
+import user from './assets/user.png';
+
+export const wishlist = [
+  {
+    name: 'Nita Syarifah',
+    infoName: 'Sahabat SMP Dinda',
+    description: `selamat menempuh hidup baru dinda dan mas indra semoga menjadi pasangan teman hidup selamanya sesurga, enjoy your little family always be happy my sweet couple!🥰🤗 amin`,
+    image: girl,
+  },
+  {
+    name: 'Ihsan Satriawan',
+    infoName: 'Rekan Kerja Indra, Tokopedia',
+    description: `Selamat menggenapkan separuh Agama ya Indra dan Dinda. Semoga melalui pernikahan ini, kalian bisa saling menguatkan dalam mengarungi kehidupan ini baik dalam kondisi senang ataupun susah. Serta semakin dekat dan konsisten dalam menjalankan perintah Agama.`,
+    image: alvia,
+  },
+  {
+    name: 'Test',
+    infoName: 'XXX, XXXX',
+    description: `Selamat menggenapkan separuh Agama ya Indra dan Dinda. Semoga melalui pernikahan ini, kalian bisa saling menguatkan dalam mengarungi kehidupan ini baik dalam kondisi senang ataupun susah. Serta semakin dekat dan konsisten dalam menjalankan perintah Agama.`,
+    image: user,
+  }
+];
