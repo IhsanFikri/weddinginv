055df31f0976543f6f96ad3e2405b199f67680e6
IhsanFikri <ihsanfikri2@gmail.com>
2022-02-02 23:46:02 +0700

    gift

--- src/components/Gift/styles.js
+++ src/components/Gift/styles.js
@@ -15,3 +15,41 @@ export const styFlex = css`
   display: flex;
   justify-content: center;
 `;
+
+export const styBoxWrapper = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 16px 0;
+
+  .img-section {
+  }
+
+  img {
+    max-width: 150px;
+    margin-right: 10px;
+  }
+
+  .text__section {
+    width: 100%;
+  }
+
+  .text__info {
+    font-weight: 500;
+    font-size: 1.2em;
+  }
+  .tengah {
+    margin: auto;
+  }
+
+  @media screen and (max-width: 480px) {
+    img {
+      max-width: 50px;
+      margin-right: 8px;
+    }
+
+    .text__info {
+      font-size: 1em;
+    }
+  }
+`;
