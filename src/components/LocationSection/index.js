372ec669e9382e7fa562571e692b66e59a657801
ihsan <ihsan@kominfo.surabaya.go.id>
2022-04-30 01:30:46 +0700

    fix ini mah

--- src/components/LocationSection/index.js
+++ src/components/LocationSection/index.js
@@ -7,7 +7,7 @@ function LocationSection() {
     <Fragment>
       <div id="fh5co-couple-story" className="fh5co-section-gray" css={styWrapper}>
         <div className="container">
-          <div className="row">
+          <div className="row text-center">
             <div className="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
               <h2 className="main-font">Lokasi Acara</h2>
               <p className="sub-title">
@@ -24,9 +24,13 @@ function LocationSection() {
                 Dsn. Tanggul, Ds. Banjartanggul, Kec. Pungging, <br />
                 Kab. Mojokerto, Jawa Timur 
               </p>
+              <button className="btn btn-sm button-nav"><a href="https://maps.app.goo.gl/NocfUsqxnXZDSRWHA" target="_blank">Lihat lokasi</a></button>
+              {/* <button className="btn btn-sm button-nav" onclick="window.open('https://maps.app.goo.gl/NocfUsqxnXZDSRWHA','_blank','resizable=yes')" >Lihat Lokasi</button> */}
             </div>
+           
+            
           </div>
-          <div className="row">
+          <div className="row text-center">
             <div className="col-md-10 col-md-offset-1">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1196.0973729875543!2d112.55151282922611!3d-7.549590670180207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd2eb91308caa6786!2zN8KwMzInNTguNSJTIDExMsKwMzMnMDcuNCJF!5e1!3m2!1sen!2sid!4v1638550803464!5m2!1sen!2sid"
@@ -40,6 +44,8 @@ function LocationSection() {
                 title="Google Maps - Zulvia & Ihsan Wedding Party"
               ></iframe>
             </div>
+            
+           
           </div>
         </div>
       </div>
--- src/components/LocationSection/styles.js
+++ src/components/LocationSection/styles.js
@@ -1,6 +1,24 @@
 import { css } from '@emotion/core';
 
 export const styWrapper = css`
+a:link {
+  color: black;
+}
+
+/* visited link */
+a:visited {
+  color: black;
+}
+
+/* mouse over link */
+a:hover {
+  color: #8E806A;
+}
+
+/* selected link */
+a:active {
+  color: blue;
+}
   @media screen and (max-width: 400px) {
     .sub-title {
       font-size: 16px;
