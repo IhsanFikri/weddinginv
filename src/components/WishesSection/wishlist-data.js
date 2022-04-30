07908968634e9a772b17478da3bc4293874d9b06
ihsan <ihsan@kominfo.surabaya.go.id>
2022-04-30 11:44:02 +0700

    add ucapan

--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -1,7 +1,21 @@
 
 import prs from './assets/prs.jpg';
-
 import ars from './assets/ars.jpg';
+import taz from './assets/taz.jpg';
+import dewi from './assets/dewi.jpg';
+import joyo from './assets/joyo.jpeg';
+import firly from './assets/firly.jpeg';
+import hurin from './assets/HURIN.jpg';
+import marini from './assets/MariniAmelia.jpg';
+import nurul from './assets/nurul.jpeg';
+import suju from './assets/suju.jpeg';
+import titus from './assets/titus.jpg';
+
+
+// import  from './assets/';
+
+
+import xxx from './assets/dewi.jpg';
 
 export const wishlist = [
   {
@@ -11,10 +25,97 @@ export const wishlist = [
     image: prs,
   },
   {
-    name: 'Arsyad',
+    name: 'Nurul',
+    infoName: 'Teman JKG',
+    description: `Happy Wedding kak Zulvia dan mas Ihsan 💐
+    Selamat menyempurnakan setengah perjalanan agama dan kehidupan baru kak. Berbahagialah kak julku.Till jannah aamiin`,
+    image: nurul,
+  },
+  {
+    name: 'suju yang cantik',
     infoName: 'Teman SMA',
-    description: `Selamat menggenapkan separuh Agama ya Ihsan dan Zulvia. Semoga melalui pernikahan ini, kalian bisa saling menguatkan dalam mengarungi kehidupan ini baik dalam kondisi senang ataupun susah. Serta semakin dekat dan konsisten dalam menjalankan perintah Agama.`,
+    description: `Selamat menikah, teman! Semoga pasanganmu sabar menghadapi tingkah laku ajaibmu, ya.`,
+    image: suju,
+  },
+
+  {
+    name: 'Mochammad Arsyad',
+    infoName: 'Konco Plek, Dolen, Mabar',
+    description: `🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰
+    Alhamdulillah, Samawa ges 😁👍🏼
+    🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰
+    🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳`,
     image: ars,
   },
+
+  {
+    name: 'Tazkya',
+    infoName: 'Teman Kuliah',
+    description: 'Selamat menjalani fase kehidupan yang baru zulvia dan ihsan. Semoga lebih bahagia dan 2 keponakan cukuplah ya.',
+    image: taz,
+  },
+
+  {
+    name: 'Fitria',
+    infoName: 'Teman Kuliah',
+    description: `Happy wedding yayukkuuu 😘 alhamdulillah setelah melewati berbagai macam lika liku kehidupan ya yukk wkwk barakallah 🤗 semoga lancar ya sampek hari H, semoga selalu diberi keberkahan dan tentunya jadi keluarga yg sakinah mawaddah warahmah 🤲🏻❤️ aamiiinnn`,
+    image: dewi,
+  },
+
+
+  {
+    name: 'Joyo',
+    infoName: 'Sahabat',
+    description: `Tidak ada keluarga yg sempurna, nanti terkadang kalian akan berdebat, berkelahi dan bahkan satu waktu berhenti berbicara satu sama lain. Namun pada akhirnya kalian bisa saling mengerti & memaafkan karena keluarga tetaplah keluarga dimana cinta akan selalu ada.`,
+    image: joyo,
+  },
+  {
+    name: 'Marimar cantiiik',
+    infoName: 'Teman kamar 5',
+    description: `Happy wedding kakak 🤗😘
+    Baarakallahu laka wa baarakaa alaika wa jamaa bainakumaa fii khoirin..
+    Semoga kakak menjadi pasangan yang sejati dunia akhirat, semoga dikaruniai keturunan yang saleh dan salehah, semoga jadi keluarga yang sakinah, mawaddah dan warahmah
+    Aamiin ya robbal'alamiin ❤❤❤`,
+    image: marini,
+  },
+
+  {
+    name: 'Adek Hurin',
+    infoName: 'Adik Unyuk',
+    description: `Assalamualaikum, hai jul(kipli). Alhamdulillah yah, setelah sekian lama berpuasa. Kini saatnya berbuka puasa dari kehausanmu dalam dunia asmara 🙊
+    Dalam setiap waktu yang akan kalian lakukan berdua, tentunya akan banyak menemukan nano nano dalam hiruk piruk rumah tetangga, maksd saya rumah tangga, Sorry. Haha
+    Doaku, semoga perjalanan kalian selalu diberikan kemudahan, keberkahan, ketenangan, dan kebahagiaan. Sakinah Mawadah warahmah untuk kalian.
+    So sweet kan kata² dariku, 😘.
+    
+    With Hate
+    Adek Hurin si Unyuk🤙`,
+    image: hurin,
+  },
+
+  {
+    name: 'Gloxynia',
+    infoName: 'Teman',
+    description: `Alhamdulillah, akhir e nikah, mugo-mugo lancar acarane tekan awal sampek akhir, awal iku akad nikah akhir iku maut(kematian), mugo-mugo nikah e diparingi berkah. Ntapss`,
+    image: titus,
+  },
+
+
+  {
+    name: 'Firly',
+    infoName: ' sahabat JKG mojosari',
+    description: `Wishing you happiness, full of love, and joy on your wedding day 🤍`,
+    image: firly,
+  },
+
+
+
+
+
+  // {
+  //   name: '',
+  //   infoName: '',
+  //   description:``,
+  //   image: xxx,
+  // },
 
 ];
