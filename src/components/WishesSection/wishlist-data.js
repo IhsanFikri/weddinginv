8787babf379cae33e17e1f4bf64d43637d2440ff
ihsan <ihsan@kominfo.surabaya.go.id>
2022-05-09 11:47:26 +0700

    add commnet

--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -10,12 +10,21 @@ import marini from './assets/MariniAmelia.jpg';
 import nurul from './assets/nurul.jpeg';
 import suju from './assets/suju.jpeg';
 import titus from './assets/titus.jpg';
+import efriza from './assets/efriza.jpg';
+import kres from './assets/kres.jpg';
+import litak from './assets/litak.jpg';
+import oc from './assets/oc.jpg';
+import tegal from './assets/tegal.jpg';
+import ve from './assets/ve.jpg';
+
+
+
 
 
 // import  from './assets/';
 
 
-import xxx from './assets/dewi.jpg';
+import xxx from './assets/f.png';
 
 export const wishlist = [
   {
@@ -107,10 +116,68 @@ export const wishlist = [
     image: firly,
   },
 
+   {
+    name: 'Efriza',
+    infoName: 'Teman Kantor',
+    description:`Happy wedding mas ihsan dan istri. Semoga diberikan kelancaran, serta dijadikan keluarga yang samawa. Aamiiin    `,
+    image: efriza,
+  },
+
+  {
+    name: 'TEGAL',
+    infoName: 'KONTRAKAN BAROKAH ',
+    description:`Mbiyen asal koe seneng, lakonono.. saiki asal bojo seneng lakonono... Samawa pakde 🙏🏻`,
+    image: tegal,
+  },
+
+  {
+    name: 'Muhammad Kresna',
+    infoName: 'Rekan Kerja',
+    description:`Selamat Menikah mas 👏 Semoga menjadi keluarga yang Sakinah Mawaddah Warohmah... Aaminn.. 🙏
+
+    وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُمْ مِنْ أَنْفُسِكُمْ أَزْوَاجًا لِتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَوَدَّةً وَرَحْمَةً إِنَّ فِي ذَلِكَ لَآيَاتٍ لِقَوْمٍ يَتَفَكَّرُونَ
+    
+    “Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.” QS Ar Rum Ayat 21
+    `,
+    image: kres,
+  },
+
+  {
+    name: 'Ochim Cantik',
+    infoName: 'Babu',
+    description:`Alhamdulillah akhirnya ada yg mau sama Ihsan wkwkwk, semoga bahagia untuk kalian berdua, penuh berkah pernikahannya, dan mampu menghadapi ujian kedepannya. Numpang ngiklan, barangkali ada yg mau nemenin saya ke kondangannya, nanti ditraktir janji jiwa 😊🙏
+    `,
+    image: oc,
+  },
 
 
+    {
+    name: 'Nurrohmah hidayati yang baik hati dan tidak sombong',
+    infoName: 'Majikan yang baik hati',
+    description:`Selamat menempuh hidup baru pada tanggal nya 🤗 berkah untuk semua dilancarkan dan diberikan kemudahan, wis iku ae dungone cukup kan ?
+.
+Selamat bersakit2 untuk aksi pertama nya , setelah itu akan ena ena di aksi2 berikutnya 👩‍❤️‍💋‍👨👩‍❤️‍💋‍👨 
+`,
+    image: xxx,
+  },
 
+ {
+    name: 'Ve',
+    infoName: 'Teman SMK',
+    description:`Happy Wedding yaa chan🐛🤝🏼👩‍❤️‍👨🌹setelah sekian purnama akhirnya akan menuju ke jenjang kehidupan yg sesungguhnya.. 🤣. Pesanku  jadilah suami juga bapak yg  ichan yg sabar,telaten dan tanggung jawab. 
+    Semoga segala urusan dan acaranya dilancarkan, dan semoga sakinah mawaddah warohmah 👩‍❤️‍👨🐛aamiin `,
+    image: ve,
+  },
 
+ {
+    name: 'Litak & Amre',
+    infoName: 'Teman Kuliah & Kerja',
+    description:`Barakallah.. Happy wedding Ihsan & Zul.. Akhirnya setelah sekian lama berhubungan dan sekarang resmi juga jadi pasangan halal.. 🥰  Wishing you both a long future together filled with love and laughter. Enjoy your journey guys!! ❤️
+    `,
+    image: litak,
+  },
+
+  
   // {
   //   name: '',
   //   infoName: '',
